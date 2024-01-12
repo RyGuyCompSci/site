dd70128a2b7df263b67ec641d96ce385525df1b1
Ryan <ryguycompsci@gmail.com>
2024-01-11 22:17:40 -0500

    Update history

--- src/components/Box.js
+++ src/components/Box.js
@@ -1,9 +1,9 @@
 const Box = ({ children }) => (
   <div>
-    <div className="d-lg-none rounded-lg p-4 bg-black big-font overflow-hidden">
+    <div className="d-lg-none rounded-lg p-4 bg-transluscent-black big-font overflow-hidden">
       {children}
     </div>
-    <div className="d-none d-lg-block rounded-lg p-5 my-5 bg-black big-font overflow-hidden">
+    <div className="d-none d-lg-block rounded-lg p-5 my-5 bg-transluscent-black big-font overflow-hidden">
       {children}
     </div>
   </div>
